c822c966b7b0a40aee56dadb248f437114ec5a70
somayeh <so.tohidi@gmail.com>
2024-11-10 08:20:59 +0330

    part3

---
+++ front/java/scripts.js
@@ -0,0 +1,12 @@
+
+console.log("conected...");
+
+function findBiggestFraction(a,b) 
+{
+    var result;
+
+a>b ? result=["a : "+a] : document.write("b : "+b);  
+}
+var a=3/4;
+var b=5/7;
+findBiggestFraction(a,b);
